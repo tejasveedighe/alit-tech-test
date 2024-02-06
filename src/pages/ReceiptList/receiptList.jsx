b649f863e6849ae8192e2932406294d6b3b20e77
tejasveedighe <tejasveedighe@gmail.com>
2024-02-06 22:03:53 +0530

    Print to excel
    used react-export-table-to-excel for converting table to excel sheet

--- src/pages/ReceiptList/receiptList.jsx
+++ src/pages/ReceiptList/receiptList.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Button, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchReceipts } from "../../redux/slices/receiptSlice";
+import { Navigate, useNavigate } from "react-router-dom";
+import { DownloadTableExcel } from "react-export-table-to-excel";
 
 const ReceiptList = () => {
+	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const { status, loading, receipts } = useSelector((store) => store.receipts);
 
@@ -15,9 +18,8 @@ const ReceiptList = () => {
 		// code for opening CRUD interface for creating new receipt record
 	};
 
-	const printReceipts = () => {
-		// code for exporting grid data to PDF or Excel
-	};
+	const tableRef = useRef();
+	const printReceipts = () => {};
 
 	const editReceipt = (id) => {
 		// code for editing receipt record with given id
@@ -27,39 +29,62 @@ const ReceiptList = () => {
 		// code for deleting receipt record with given id
 	};
 
+	const refreshReceipts = useCallback(() => {
+		dispatch(fetchReceipts());
+	}, [dispatch]);
+
 	return (
-		<div className="container vh-100">
-			<Table striped bordered hover>
-				<thead>
-					<tr>
-						<th>Receipt No.</th>
-						<th>Receipt Date</th>
-						<th>Person Name</th>
-						<th>Total Qty</th>
-						<th>Add</th>
-						<th>Net Amount</th>
-						<th>Remarks</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				{loading ? (
-					status === "rejected" ? (
-						<div className="text-danger">
-							Failed To load the data please try later
-						</div>
+		<main className="container d-flex align-items-center justify-content-center vh-100">
+			<section>
+				<h1>Receipts</h1>
+				<div className="float-end mb-2 d-flex gap-2 align-items-center">
+					<Button variant="success" onClick={addReceipt}>
+						Add Receipt
+					</Button>
+					<DownloadTableExcel
+						filename="users table"
+						sheet="users"
+						currentTableRef={tableRef.current}
+					>
+						<Button variant="primary">Print</Button>
+					</DownloadTableExcel>
+					<Button variant="secondary" onClick={refreshReceipts}>
+						Refresh
+					</Button>
+					<Button variant="danger" onClick={() => navigate("/")}>
+						Exit
+					</Button>
+				</div>
+				<Table ref={tableRef} striped bordered hover>
+					<thead>
+						<tr>
+							<th>Sr No.</th>
+							<th>Receipt No.</th>
+							<th>Receipt Date</th>
+							<th>Person Name</th>
+							<th>Total Qty</th>
+							<th>Net Amount</th>
+							<th>Remarks</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					{loading ? (
+						status === "rejected" ? (
+							<div className="text-danger">
+								Failed To load the data please try later
+							</div>
+						) : (
+							<div>Loading...</div>
+						)
 					) : (
-						<div>Loading...</div>
-					)
-				) : (
-					<>
 						<tbody>
-							{receipts.map((receipt) => (
-								<tr key={receipt.id}>
-									<td>{receipt.receiptNo}</td>
-									<td>{receipt.receiptDate}</td>
-									<td>{receipt.personName}</td>
+							{receipts.map((receipt, index) => (
+								<tr key={crypto.randomUUID()}>
+									<td>{index + 1}</td>
+									<td>{receipt.billNo}</td>
+									<td>{new Date(receipt.billDate).toLocaleDateString()}</td>
+									<td>{receipt.customerName}</td>
 									<td>{receipt.totalQty}</td>
-									<td>{receipt.add}</td>
 									<td>{receipt.netAmount}</td>
 									<td>{receipt.remarks}</td>
 									<td>
@@ -80,22 +105,13 @@ const ReceiptList = () => {
 									</td>
 								</tr>
 							))}
-						</tbody>{" "}
-					</>
-				)}
-			</Table>
-			<div className="d-flex justify-content-between align-items-center">
-				<div>
-					<Button variant="success" onClick={addReceipt}>
-						Add Receipt
-					</Button>
-					<Button variant="secondary" onClick={printReceipts}>
-						Print
-					</Button>
-				</div>
-				<div>Showing {receipts.length} records</div>
-			</div>
-		</div>
+						</tbody>
+					)}
+				</Table>
+
+				<div className="float-end">Showing {receipts.length} records</div>
+			</section>
+		</main>
 	);
 };
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,7 @@ export const BASE_URL = "https://reacttestprojectapi.azurewebsites.net/api/";
 
 export const Axios = axios.create({
 	baseURL: BASE_URL,
+	timeout: 1000,
 	headers: {
 		Authorization: `Bearer ${localStorage.getItem("authToken")}`,
 	},
