6ea8dcdd298bfedeaba676498e236b7a31ba6dda
tejasveedighe <tejasveedighe@gmail.com>
2024-02-08 11:29:40 +0530

    Axios token instance interceptor added to use token

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,19 @@ export const BASE_URL = "https://reacttestprojectapi.azurewebsites.net/api/";
 
 export const Axios = axios.create({
 	baseURL: BASE_URL,
-	headers: {
-		Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-	},
 });
+
+Axios.interceptors.request.use(
+	(config) => {
+		const authToken = localStorage.getItem("authToken");
+
+		if (authToken) {
+			config.headers.Authorization = `Bearer ${authToken}`;
+		}
+
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
